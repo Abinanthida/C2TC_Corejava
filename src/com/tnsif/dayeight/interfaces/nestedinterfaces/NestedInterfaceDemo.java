fc81f101d7034323d2ed55bdef3f76c832879d43
sachin1ab <sachinthara30@gmail.com>
2025-10-21 21:15:30 +0530

    Day 4 - Day 10

---
+++ src/com/tnsif/dayeight/interfaces/nestedinterfaces/NestedInterfaceDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.dayeight.interfaces.nestedinterfaces;
+
+	public class NestedInterfaceDemo {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			NestedInterface obj = new NestedInterface();
+			obj.print();
+			System.out.println(NestedInterface.id);
+
+		}
+
+	}
+
