2a49bbead9510c8613119b3ebd8304497767375b
Abinanthida <abigowrish2004@gmail.com>
2025-10-31 14:45:54 +0530

    Day 11-19 completed

---
+++ src/com/tnsif/daynineteen/IsOdd.java
@@ -0,0 +1,7 @@
+package com.tnsif.daynineteen;
+
+@FunctionalInterface
+interface IsOdd{
+	public boolean CheckOdd(int a);
+}
+
---
+++ src/com/tnsif/daynineteen/v1/Palindrome.java
@@ -0,0 +1,4 @@
+package com.tnsif.daynineteen.v1;
+public interface Palindrome<T> {
+	boolean checkPalindrome(T data);
+}
